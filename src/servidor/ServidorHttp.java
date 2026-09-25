f1a4813b63b39cd9276dc0256af83683dc9c67cd
Maria Eduarda Campos <163140974+Mescxll@users.noreply.github.com>
2026-09-25 10:43:35 -0300

    adiciona chamada de Calculo ao servidor

--- src/servidor/ServidorHttp.java
+++ src/servidor/ServidorHttp.java
@@ -88,7 +88,7 @@ public class ServidorHttp {
 
         for (int i = 0; i < caminho.size(); i++) {
             String ponto = caminho.get(i);
-            int[] coordenadas = getCoordenadas(matriz, ponto);
+            int[] coordenadas = Calculo.getCoordenadas(matriz, ponto);
 
             if (i > 0) {
                 acumulado += calculaDistancia(matriz, caminho.get(i - 1), ponto);
@@ -120,20 +120,7 @@ public class ServidorHttp {
     }
 
     private static int calculaDistancia(String[][] matriz, String origem, String destino) {
-        int[] c1 = getCoordenadas(matriz, origem);
-        int[] c2 = getCoordenadas(matriz, destino);
-        return Math.abs(c1[0] - c2[0]) + Math.abs(c1[1] - c2[1]);
-    }
-
-    private static int[] getCoordenadas(String[][] matriz, String valor) {
-        for (int lin = 0; lin < matriz.length; lin++) {
-            for (int col = 0; col < matriz[lin].length; col++) {
-                if (valor.equals(matriz[lin][col])) {
-                    return new int[]{col, lin};
-                }
-            }
-        }
-        return new int[]{-1, -1};
+        return Calculo.calculaDistancia (origem, destino);
     }
 
     private static void tratarEstatico(HttpExchange exchange) throws IOException {
